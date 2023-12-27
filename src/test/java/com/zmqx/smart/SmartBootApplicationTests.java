c68517bc9a6e133e32f40614ef6cd9841fadb99d
AbelEthan <abelethan@vip.qq.com>
2023-12-27 17:17:15 +0800

    Initial commit

---
+++ src/test/java/com/zmqx/smart/SmartBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmqx.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
